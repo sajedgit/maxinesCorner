a986c90f0bb12e53d87c35af657a0b6dc5ae0ec8
Sajed Ahmed Khan <sajedaiub@gmail.com>
2019-09-17 19:20:52 +0600

    some updates after confusion

--- wp-content/themes/MaxinesCorner/sidebar-page-menu.php
+++ wp-content/themes/MaxinesCorner/sidebar-page-menu.php
@@ -6,7 +6,7 @@
 			<li><a href="<?php echo home_url(); ?>#about">About</a></li>          
 			<li><a href="<?php echo home_url(); ?>#pricing">Weight Loss</a></li>
             <li><a href="<?php echo home_url(); ?>#Contact">Contact</a></li>    
-			<li><a href="<?php home_url('') ?>maxinescorner-store">MaxinesCorner Store</a></li>
+			<li><a href="<?php site_url() ?>maxinescorner-store">MaxinesCorner Store</a></li>
 			<li><a href="https://amzn.to/2PcWuk5">Amazon Store</a></li>
            <!-- <li><a href="#pricing">Pricing</a></li>-->
             <!--<li class="dropdown">
